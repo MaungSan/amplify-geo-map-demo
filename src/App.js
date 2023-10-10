2e1b3bf56bc80bb3ad97fab4de0d52b6ed2feb9b
Maung San <msan001@gmail.com>
2023-10-10 11:54:44 -0400

    added router and protected path

--- src/App.js
+++ src/App.js
@@ -1,79 +1,52 @@
-import { MapView } from "@aws-amplify/ui-react-geo";
+//App.js
+import { Authenticator } from "@aws-amplify/ui-react";
+
+import { Protected } from "./components/Protected";
+import { RequireAuth } from "./components/RequireAuth";
+import { Login } from "./components/Login";
+import { ProtectedSecond } from "./components/ProtectSecond";
+import { Home } from "./components/Home";
+import { Layout } from "./components/Layout";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import { Hikes, StandardCard } from "./ui-components";
-import { listHikes } from "./graphql/queries";
-import { useState, useEffect } from "react";
-import { API } from "aws-amplify";
-import { Marker, Popup } from "react-map-gl";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-function MarkerWithPopup({ hike, latitude, longitude }) {
-  const [showPopup, setShowPopup] = useState(false);
 
-  const handleMarkerClick = (e) => {
-    e.originalEvent.stopPropagation();
-    setShowPopup(true);
-  };
+function MyRoutes() {
   return (
-    <>
-      <Marker
-        latitude={hike.lat}
-        longitude={hike.long}
-        key={hike.id}
-        onClick={handleMarkerClick}
-      />
-      {showPopup && (
-        <Popup
-          latitude={latitude}
-          longitude={longitude}
-          offset={{ bottom: [0, -40] }}
-          onClose={() => setShowPopup(false)}
-          maxWidth="95%"
-          closeOnMove
-        >
-          <StandardCard hike={hike}></StandardCard>
-        </Popup>
-      )}
-    </>
-  );
-}
-function App() {
-  const [hikes, setHikes] = useState([]);
-  const pageSize = 6;
-  const variables = {
-    limit: pageSize,
-  };
-  useEffect(() => {
-    const getHikes = async () => {
-      const result = (
-        await API.graphql({
-          query: listHikes,
-          variables,
-        })
-      ).data.listHikes;
-      setHikes(result.items);
-    };
-    getHikes();
-  }, []);
-  return (
-    <>
-      <MapView
-        initialViewState={{
-          latitude: 39.113014,
-          longitude: -105.358887,
-          zoom: 7,
-        }}
-      >
-        {hikes.map((hike) => (
-          <MarkerWithPopup
-            latitude={hike.lat}
-            longitude={hike.long}
-            hike={hike}
-            key={hike.id}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <Protected />
+              </RequireAuth>
+            }
           />
-        ))}
-      </MapView>
-    </>
+          <Route
+            path="/protected2"
+            element={
+              <RequireAuth>
+                <ProtectedSecond />
+              </RequireAuth>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default withAuthenticator(App);
+function App() {
+  return (
+    <Authenticator.Provider>
+      <MyRoutes />
+    </Authenticator.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,78 @@
+import { MapView } from "@aws-amplify/ui-react-geo";
+import "./App.css";
+import { Hikes, StandardCard } from "../ui-components";
+import { listHikes } from "./graphql/queries";
+import { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { Marker, Popup } from "react-map-gl";
+function MarkerWithPopup({ hike, latitude, longitude }) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMarkerClick = (e) => {
+    e.originalEvent.stopPropagation();
+    setShowPopup(true);
+  };
+  return (
+    <>
+      <Marker
+        latitude={hike.lat}
+        longitude={hike.long}
+        key={hike.id}
+        onClick={handleMarkerClick}
+      />
+      {showPopup && (
+        <Popup
+          latitude={latitude}
+          longitude={longitude}
+          offset={{ bottom: [0, -40] }}
+          onClose={() => setShowPopup(false)}
+          maxWidth="95%"
+          closeOnMove
+        >
+          <StandardCard hike={hike}></StandardCard>
+        </Popup>
+      )}
+    </>
+  );
+}
+function Map() {
+  const [hikes, setHikes] = useState([]);
+  const pageSize = 6;
+  const variables = {
+    limit: pageSize,
+  };
+  useEffect(() => {
+    const getHikes = async () => {
+      const result = (
+        await API.graphql({
+          query: listHikes,
+          variables,
+        })
+      ).data.listHikes;
+      setHikes(result.items);
+    };
+    getHikes();
+  }, []);
+  return (
+    <>
+      <MapView
+        initialViewState={{
+          latitude: 39.113014,
+          longitude: -105.358887,
+          zoom: 7,
+        }}
+      >
+        {hikes.map((hike) => (
+          <MarkerWithPopup
+            latitude={hike.lat}
+            longitude={hike.long}
+            hike={hike}
+            key={hike.id}
+          />
+        ))}
+      </MapView>
+    </>
+  );
+}
+
+export default Map;
